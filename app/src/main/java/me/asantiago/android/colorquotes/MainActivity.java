56687bb7e6324d34001e64386ee5dcdda6bab6a0
Amador368 <amadorsantiago368@gmail.com>
2016-11-21 16:39:46 -0600

    creating arrays

--- app/src/main/java/me/asantiago/android/colorquotes/MainActivity.java
+++ app/src/main/java/me/asantiago/android/colorquotes/MainActivity.java
@@ -2,10 +2,12 @@ package me.asantiago.android.colorquotes;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends Activity {
 
@@ -14,6 +16,25 @@ public class MainActivity extends Activity {
     private TextView citaTextView;
     private TextView autorTextView;
     private Button nuevaFraseButton;
+
+    private String [] quotes = new String[5];
+    private String [] autors = new String[5];
+
+    autors[0] = "Walt Disney";
+    quotes[0] = "Aprendí que lo difícil no es llegar a la cima, sino jamás dejar de subir";
+
+    autors[1] = "Albert Einstein";
+    quotes[1] = "La imaginación es más importante que el conocimiento";
+
+
+    autors[2] = "Steve Jobs";
+    quotes[2] = "Tu tiempo es limitado, así que no lo desperdicies viviendo la vida de otra persona";
+
+    autors[3] = "Albert Camus";
+    quotes[3] = "El éxito es fácil de obtener. Lo difícil es merecerlo.";
+
+    autors[4] = "Irving Berlin";
+    quotes[4] = "El sabio no dice lo que sabe, y el necio no sabe lo que dice.";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +54,15 @@ public class MainActivity extends Activity {
     }
 
     public void newQuote(View view){
-        Log.d(TAG,"Upps Touching");
+
+    }
+
+    private int generateRandomNumber(int max){
+        Random random = new Random();
+        random.setSeed(System.currentTimeMillis());
+
+        int randomNumber = random.nextInt(max);
+        return randomNumber;
     }
 
 }
---
+++ app/src/main/java/me/asantiago/android/colorquotes/Util.java
@@ -0,0 +1,8 @@
+package me.asantiago.android.colorquotes;
+
+/**
+ * Created by Amador on 21/11/2016.
+ */
+
+public class Util {
+}
