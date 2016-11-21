e3367b237c0f1053b6b4f9ba5f09ff3218a34281
Amador368 <amadorsantiago368@gmail.com>
2016-11-21 02:05:33 -0600

    adding the code for declare the views

--- app/src/main/java/me/asantiago/android/colorquotes/MainActivity.java
+++ app/src/main/java/me/asantiago/android/colorquotes/MainActivity.java
@@ -2,12 +2,22 @@ package me.asantiago.android.colorquotes;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    private TextView citaTextView;
+    private TextView autorTextView;
+    private Button nuevaFraseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        citaTextView = (TextView) findViewById(R.id.citaTextView);
+        autorTextView = (TextView) findViewById(R.id.autorTextView);
+        nuevaFraseButton = (Button) findViewById(R.id.nuevaFraseButton);
     }
 }
