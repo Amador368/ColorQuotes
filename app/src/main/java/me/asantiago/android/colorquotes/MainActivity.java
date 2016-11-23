92806b6b63e05273e1339e6fbc0bed9ebca0368b
Amador368 <amadorsantiago368@gmail.com>
2016-11-23 02:00:53 -0600

    displaying random quotes

--- app/src/main/java/me/asantiago/android/colorquotes/MainActivity.java
+++ app/src/main/java/me/asantiago/android/colorquotes/MainActivity.java
@@ -3,6 +3,7 @@ package me.asantiago.android.colorquotes;
 import android.app.Activity;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -17,27 +18,11 @@ public class MainActivity extends Activity {
     private TextView autorTextView;
     private Button nuevaFraseButton;
 
-    private String [] quotes = new String[5];
-    private String [] autors = new String[5];
+    private String[] quotes;
 
-    autors[0] = "Walt Disney";
-    quotes[0] = "Aprendí que lo difícil no es llegar a la cima, sino jamás dejar de subir";
+    private String[] autors;
 
-    autors[1] = "Albert Einstein";
-    quotes[1] = "La imaginación es más importante que el conocimiento";
-
-
-    autors[2] = "Steve Jobs";
-    quotes[2] = "Tu tiempo es limitado, así que no lo desperdicies viviendo la vida de otra persona";
-
-    autors[3] = "Albert Camus";
-    quotes[3] = "El éxito es fácil de obtener. Lo difícil es merecerlo.";
-
-    autors[4] = "Irving Berlin";
-    quotes[4] = "El sabio no dice lo que sabe, y el necio no sabe lo que dice.";
-
-
-    @Override
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -45,6 +30,26 @@ public class MainActivity extends Activity {
         citaTextView = (TextView) findViewById(R.id.citaTextView);
         autorTextView = (TextView) findViewById(R.id.autorTextView);
         nuevaFraseButton = (Button) findViewById(R.id.nuevaFraseButton);
+
+        quotes = new String[5];
+        autors = new String[5];
+
+        autors[0] = "Walt Disney";
+        quotes[0] = "Aprendí que lo difícil no es llegar a la cima, sino jamás dejar de subir";
+
+        autors[1] = "Albert Einstein";
+        quotes[1] = "La imaginación es más importante que el conocimiento";
+
+
+        autors[2] = "Steve Jobs";
+        quotes[2] = "Tu tiempo es limitado, así que no lo desperdicies viviendo la vida de otra persona";
+
+        autors[3] = "Albert Camus";
+        quotes[3] = "El éxito es fácil de obtener. Lo difícil es merecerlo.";
+
+        autors[4] = "Irving Berlin";
+        quotes[4] = "El sabio no dice lo que sabe, y el necio no sabe lo que dice.";
+
 
         /*nuevaFraseButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,6 +67,8 @@ public class MainActivity extends Activity {
 
         citaTextView.setText(cita);
         autorTextView.setText(autor);
+        Log.d(TAG,cita);
+        Log.d(TAG,autor);
     }
 
     private int generateRandomNumber(int max){
