b0c0bec3cd4f4965b74ada602e222a990c58622a
Amador368 <amadorsantiago368@gmail.com>
2016-12-04 11:33:53 -0600

    creating class for Cita and refactor code

--- app/src/main/java/me/asantiago/android/colorquotes/MainActivity.java
+++ app/src/main/java/me/asantiago/android/colorquotes/MainActivity.java
@@ -14,9 +14,6 @@ public class MainActivity extends Activity {
     private TextView autorTextView;
     private Button nuevaFraseButton;
 
-    private String[] quotes;
-
-    private String[] autors;
 
         @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,35 +24,14 @@ public class MainActivity extends Activity {
         autorTextView = (TextView) findViewById(R.id.autorTextView);
         nuevaFraseButton = (Button) findViewById(R.id.nuevaFraseButton);
 
-        quotes = new String[5];
-        autors = new String[5];
-
-        autors[0] = "Walt Disney";
-        quotes[0] = "Aprendí que lo difícil no es llegar a la cima, sino jamás dejar de subir";
-
-        autors[1] = "Albert Einstein";
-        quotes[1] = "La imaginación es más importante que el conocimiento";
-
-
-        autors[2] = "Steve Jobs";
-        quotes[2] = "Tu tiempo es limitado, así que no lo desperdicies viviendo la vida de otra persona";
-
-        autors[3] = "Albert Camus";
-        quotes[3] = "El éxito es fácil de obtener. Lo difícil es merecerlo.";
-
-        autors[4] = "Irving Berlin";
-        quotes[4] = "El sabio no dice lo que sabe, y el necio no sabe lo que dice.";
-
     }
 
     public void newQuote(View view){
 
-        int randomGenerated = Utils.generateRandomNumber(quotes.length);
-        String cita = quotes[randomGenerated];
-        String autor = autors[randomGenerated];
-
-        citaTextView.setText(cita);
-        autorTextView.setText(autor);
+        QuotesGenerator quotesGenerator = new QuotesGenerator();
+        Cita cita = quotesGenerator.obtainRandomQuote();
+        citaTextView.setText(cita.getText());
+        autorTextView.setText(cita.getAutor());
     }
 
 }
---
+++ app/src/main/java/me/asantiago/android/colorquotes/QuotesGenerator.java
@@ -0,0 +1,42 @@
+package me.asantiago.android.colorquotes;
+
+/**
+ * Created by asantiago on 04/12/16.
+ */
+
+public class QuotesGenerator {
+
+    private Cita[] quotes;
+
+    public QuotesGenerator(){
+
+        quotes = new Cita[5];
+
+        quotes[0] = new Cita();
+        quotes[0].setAutor("Walt Disney");
+        quotes[0].setText("Aprendí que lo difícil no es llegar a la cima, sino jamás dejar de subir");
+
+        quotes[1] = new Cita();
+        quotes[1].setAutor("Albert Einstein");
+        quotes[1].setText("La imaginación es más importante que el conocimiento");
+
+        quotes[2] = new Cita();
+        quotes[2].setAutor("Steve Jobs");
+        quotes[2].setText("Tu tiempo es limitado, así que no lo desperdicies viviendo la vida de otra persona");
+
+        quotes[3] = new Cita();
+        quotes[3].setAutor("Albert Camus");
+        quotes[3].setText("El exito es facil de obtener. Lo dificil es merecerlo.");
+
+        quotes[4] = new Cita();
+        quotes[4].setAutor("Irving Berlin");
+        quotes[4].setText("El sabio no dice lo que sabe, y el necio no sabe lo que dice.");
+
+    }
+
+    public Cita obtainRandomQuote(){
+        int randomGenerated = Utils.generateRandomNumber(quotes.length);
+        return quotes[randomGenerated];
+    }
+
+}
